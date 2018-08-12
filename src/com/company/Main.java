bffe230064187f75c1b7a6d97a013ea83f67e257
adamstom <adamstom@interia.pl>
2018-08-12 10:29:43 +0200

    dodanie metody do wyświetlania tekstu

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,15 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
+        wyswietlTekst("TEST z innej metody");
     }
+
+
+
+    public static void wyswietlTekst(String napis) {
+//        System.out.println(String.format("Tekst: {0}",napis));
+        System.out.println("Tekst: "+napis);
+    }
+
 }
